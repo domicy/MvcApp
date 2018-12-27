9924ea54ffea09d8da3ef10eaae8abe6456294f6
domicy <domaspuras@yahoo.com>
2018-12-27 21:03:07 +0200

    Added database seed for movies

--- MvcMovie/MvcMovie/Migrations/Configuration.cs
+++ MvcMovie/MvcMovie/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace MvcMovie.Migrations
 {
+    using MvcMovie.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -19,6 +20,40 @@ namespace MvcMovie.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            context.Movies.AddOrUpdate(i => i.Title,
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-1-11"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M
+                },
+
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M
+                }
+            );
         }
     }
 }
