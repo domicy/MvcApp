2f053c6f5a8cbdee9f91801ffe70a52bc3aee378
domicy <domaspuras@yahoo.com>
2018-12-27 02:28:15 +0200

    Added User table

---
+++ MvcMovie/MvcMovie/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
